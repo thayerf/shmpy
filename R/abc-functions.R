4667cd9d053e5908d65779bb7c400505b88f7a3b
Julia Fukuyama <julia.fukuyama@gmail.com>
2019-03-29 10:52:13 -0400

    documentation update

--- R/abc-functions.R
+++ R/abc-functions.R
@@ -4,7 +4,7 @@
 #' @param sim_statistics Statistics corresponding to each parameter set.
 #' @param obs_statistics Statistics for the observed data.
 #' @param match_tol What fraction of the samples to keep.
-#'
+#' @export
 #' @return A matrix, columns representing parameters, of accepted parameters.
 abc_rej <- function(sim_params, sim_statistics, obs_statistics, match_tol = .01) {
     obs_sim_dists = apply(sim_statistics, 1, function(x) sum((x - obs_statistics)^2))
